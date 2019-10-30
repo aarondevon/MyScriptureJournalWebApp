2affd3af99c0f31c41c3cb94fc61144d618ef62e
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-10-29 20:06:57 -0500

    Add project files.

---
+++ MyScriptureJournal/Models/JournalEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class JournalEntry
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EntryDate { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public string Notes { get; set; }
+    }
+}
